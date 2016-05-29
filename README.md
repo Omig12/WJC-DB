44233d30b4ce300c58851dff08267a8aa8466be3
Omig12 <israelodilan@gmail.com>
2016-05-29 19:59:34 -0400

    Adds some information to Readme file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wjc.githubpages.io
-Sample Database
+# ada.uprrp.edu/~idilan/WJC-DB/index.php
+
+_Sample Database User Interface_
+
+This database aims to relate multiple wildlife reserves that protect endemic puertorrican species that are threatened.
+
+ - Users must be familiar with SQL Syntax.
+ - A radial button must be selected for each query _(except the custom query box)_
+ - When utilizing the list elements that end with **Like** the custom query box is used to as an input field for the queried item.
--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
 	<title>WJC - Wildlife Journal for Conservation </title>
-	<link rel="icon" href="http://static1.squarespace.com/static/5067061fc4aa71efcf50abc1/532e0065e4b0f59c297a79c4/532e0a87e4b0011333cb20ca/1395526281114/OK-Icon-Wildlife.png" sizes="16x16 32x32" > 
+	<link rel="icon" href="http://static1.squarespace.com/static/5067061fc4aa71efcf50abc1/532e0065e4b0f59c297a79c4/532e0a87e4b0011333cb20ca/1395526281114/OK-Icon-Wildlife.png" sizes="16x16 32x32" >
 	<meta charset="utf-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
-<body> 
+<body>
 	<div class="container-fluid">
 		<!-- Body -->
-		<div class="navbar" style="background: rgba(63, 121, 78, 0.68); border-bottom:1px solid #666;  box-shadow:.1em .3em .3em #666; "> 
+		<div class="navbar" style="background: rgba(63, 121, 78, 0.68); border-bottom:1px solid #666;  box-shadow:.1em .3em .3em #666; ">
 			<h1 class="navbar" style="padding-left: 1em;"> Wildlife Journal for Conservation </h1>
 			<p style="padding-left: 3em; color: #F5F5F8;"> Proyecto para CCOM 4027 <br> Creado por Israel O. Dilán y Juan Lugo</p>
 		</div>
@@ -45,7 +45,7 @@
 								<option value='A_Like'> Animal Name Like </option>
 								<option value='R_Like'> Reserve Name Like </option>
 						</select><br><br>
-						
+
 					Custom Query: <input type="text" name="Custom" placeholder="SELECT (Attribute) FROM (Entity) WHERE (Condition);" style="width:50%;"><br><br>
 					<input type="submit" value="Query WJC">
 				</form>
@@ -58,15 +58,15 @@
 						$db_name = 'WJC';
 						// Open Connection
 						$conn = mysqli_connect($db_host,$db_username,$db_pass,$db_name);
-							
+
 						// Check connection
 						if($conn->connect_error) {
 				     		die('<br>Could not connect: ' . mysqli_error().'<br>');
-				   		} 
+				   		}
 				   		else {
 				 			echo '<br>Connected successfully<br>';
-				  		}	
-	
+				  		}
+
 						// List option
 				  		$List = $_POST['Queries'];
 
@@ -84,7 +84,7 @@
 						else if (isset($_POST['Quantity'])) {
 							$sql = "SELECT speciesName as Name, sum(quantity) as Quantity from Population natural join Animal group by speciesID;";
 							$result = mysqli_query($conn, $sql);
-						}	
+						}
 						// Show Animal Table
 						else if (isset($_POST['Animal'])) {
 							$sql = "SELECT * FROM Animal;";
@@ -113,7 +113,7 @@
 						// Show Selction From list
 						else if (isset($_POST['List'])and isset($_POST['Queries'])) {
 							if ($_POST['Queries'] == 'Default') {
-								echo "<script type='text/javascript'> 
+								echo "<script type='text/javascript'>
 										alert('Please select a value from list.');
 									</script>";
 							}
@@ -132,7 +132,7 @@
 							else if ($_POST['Queries'] == 'Coexists') {
 									$sql = "SELECT speciesName as Extra_Hazards FROM Animal WHERE speciesID in (SELECT speciesID FROM (Hazard natural join Population) WHERE reserveID in (SELECT reserveID FROM (Hazard join Population) WHERE preyID = Population.speciesID));";
 									$result = mysqli_query($conn, $sql);
-								}	
+								}
 							else if ($_POST['Queries'] == 'IMG') {
 								echo '<img src="https://upload.wikimedia.org/wikipedia/en/e/e4/Anolis_roosevelti.jpg">';
 							}
@@ -142,13 +142,13 @@
 								$sql = "SELECT * FROM Animal WHERE speciesID like '%".$_POST['Custom']."%' OR speciesName like '%".$_POST['Custom']."%' OR commonName Like '%".$_POST['Custom']."%' OR class Like '%".$_POST['Custom']."%';";
 								$result = mysqli_query($conn, $sql);
 								}
-							}	
+							}
 							else if ($_POST['Queries'] == 'R_Like') {
 								if ($_POST['Custom']){
 									echo "<h4>Searched: ".$_POST['Custom']."</h4>";
 									$sql = "SELECT * FROM Reserve WHERE ReserveName like '%".$_POST['Custom']."%' OR city like '%".$_POST['Custom']."%' OR reserveID like '%".$_POST['Custom']."%';";
 									$result = mysqli_query($conn, $sql);
-								}	
+								}
 								else if ($_POST['Custom'] == ''){
 									echo "<h1>Please enter an animal name into Custom Query box.</h1>";
 								}
@@ -168,7 +168,7 @@
 							  }
 						}
 
-						// Fill table 
+						// Fill table
 						echo "<TR>";
 						// Generate Headers
 						$fieldinfo=mysqli_fetch_fields($result);
@@ -179,10 +179,10 @@
 						// Populate table
 						while($record = mysqli_fetch_array($result)){
 							echo "<tr>";
-							for ($i=0; $i < mysqli_field_count($conn); $i++) { 
+							for ($i=0; $i < mysqli_field_count($conn); $i++) {
 								echo "<td>".$record[$i]."</td>";
 							};
-							echo "</tr>";	
+							echo "</tr>";
 						}
 
 						$result->free();
@@ -190,10 +190,6 @@
 					?>
 				</table>
 			</div>
-		</div>
-		<div background='blue'>
-				<h2 class="navbar-header"> Pagina </h2>
-		</div>
 		</div>
 	</div>
 </body>
